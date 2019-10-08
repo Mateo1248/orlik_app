edaed93d1b91b6b61f0be1d36bd8b91a4317fcb2
Marcin Umiński <marcinuminski1998@gmail.com>
2019-10-08 22:32:02 +0200

    Initial commit

---
+++ src/main/java/com/orlikteam/orlikbackend/OrlikBackendApplication.java
@@ -0,0 +1,13 @@
+package com.orlikteam.orlikbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrlikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrlikBackendApplication.class, args);
+	}
+
+}
