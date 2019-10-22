2ffd368551ac7fbe6b63e47e8999960e5f0112d8
Arek Bochenek <piotrp1287@gmail.com>
2019-10-22 19:12:59 +0200

    DB changed to be more friendly handled by app and server

--- src/database/creatingDB.sql
+++ src/database/creatingDB.sql
@@ -14,10 +14,8 @@ create table users (
 create table pitches (
 	pitch_id int auto_increment not null primary key,
 	pitch_name varchar(70) not null,
-	latitude_1st smallint not null,
-	longitude_1st smallint not null,
-    	latitude_2st int not null,
-    	longitude_2st int not null);
+	latitude decimal not null,
+	longitude decimal not null);
 
 
 create table reservations (
@@ -73,6 +71,56 @@ create trigger reservation_validation
 		end if;
 	end;$$
 delimiter ;*/
+
+
+
+
+
+
+
+
+/* validation triggers was created, but two-level validation in mobile app and java server is enough to care about data consistency */
+
+
+/*delimiter $$
+drop trigger if exists playground_validation$$
+create trigger playground_validation
+	before insert on playgrounds
+    for each row
+    begin
+		if(new.latitude_1st<0 or new.latitude_1st>180) then
+			signal sqlstate '40000'
+			set message_text = 'Latitude first coordinate should be value from [0, 180]';
+		end if;
+        if(new.longitude_1st<0 or new.longitude_1st>180) then
+			signal sqlstate '40000'
+			set message_text = 'Longitude first coordinate should be value from [0, 180]';
+		end if;
+        if(new.latitude_2st<0) then
+			signal sqlstate '40000'
+			set message_text = 'Latitude second coordinate should be value greater than 0';
+		end if;
+        if(new.longitude_2st<0) then
+			signal sqlstate '40000'
+			set message_text = 'Longitude second coordinate should be value greater than 0';
+		end if;
+	end;$$
+delimiter ;
+
+
+delimiter $$
+drop trigger if exists reservation_validation $$
+create trigger reservation_validation
+	before insert on reservations
+    for each row
+    begin
+		if(new.reservation_date<curdate()) then
+			signal sqlstate '40000'
+			set message_text = 'Reservation may be done only in future, not in the past';
+		end if;
+	end;$$
+delimiter ;*/
+
 
 
 
