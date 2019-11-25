8911abd3f28fba5d9445885e4c2220d4530883e9
Arek Bochenek <piotrp1287@gmail.com>
2019-11-25 22:52:12 +0100

    test of docs

--- src/main/java/com/orlikteam/orlikbackend/user/UserResource.java
+++ src/main/java/com/orlikteam/orlikbackend/user/UserResource.java
@@ -8,18 +8,26 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+
 @RestController
 @RequestMapping("/users")
+
 public class UserResource {
 
     private final UserService userService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
 
     public UserResource(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
+    /**
+     * method is used to send a POST request to database (to send object of user to service)
+     * @param user is a object made from given by user login and password (which is encoding here)
+     * @return login of already added user got from service
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserLoginDto addUser(@RequestBody @Validated UserDto user) {
@@ -27,16 +35,31 @@ public class UserResource {
         return userService.addUser(user);
     }
 
+    /**
+     * method is used to send a DELETE request to database (to send login to service)
+     * @param userLogin is a login of user who we are eager to delete from app
+     */
     @DeleteMapping("/{userLogin}")
     public void removeUser(@PathVariable String userLogin) {
         userService.removeUser(userLogin);
     }
 
+
+    /**
+     * method is used to send a GET request to database (to send login to service)
+     * @param userLogin is a login of user who we are eager to get from app
+     */
     @GetMapping("/{userLogin}")
     public String getUser(@PathVariable String userLogin) {
         return userService.getUser(userLogin);
     }
 
+
+    /**
+     * method is used to send a PATCH request to database (to send object of user to service)
+     * @param user is a object of user made from currently logged user's login and new password (which is encoding here)
+     * @return login of user who already updated his password
+     */
     @PatchMapping
     @ResponseStatus(HttpStatus.OK)
     public void updateUser(@RequestBody @Validated UserDto user) {
